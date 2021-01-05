2da8fcd8134c2aca7c797c11554ef5fc92090b42
Ciprian Ionescu <me@ciprian-ionescu.com>
2021-01-05 18:00:23 +0200

    custom user model

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+from .forms import UserCreationForm, UserChangeForm
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+
+
+admin.site.register(User, CustomUserAdmin)
